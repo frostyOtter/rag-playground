a6ec5a0e0777157c52b74b8910a680363123b23c
frostyOtter <binary.hamlet147@gmail.com>
2025-05-04 13:25:13 +0700

    feat: add scripts to run data processors

---
+++ Scripts/run_pymupdf4llm_processor.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+uv run src/data_service/pymupdf4llm_processor.py data/INFORMATION-TECHNOLOGY/10089434.pdf extracted_contexts
---
+++ Scripts/run_pymupdf_processor.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Run the PyMuPDF processor with the provided input and output paths
+uv run src/data_service/pymupdf_processor.py data/INFORMATION-TECHNOLOGY/10089434.pdf extracted_contexts
